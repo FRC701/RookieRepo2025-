01cee9e3fc839d9a1933c97d02c87f20f0290446
Zophya M <zophyam@vandenrobotics.com>
2024-10-07 16:57:44 -0700

    Worked on SecondZophyaFeeder subsystem

--- RookieCoding/src/main/java/frc/robot/subsystems/SecondZophyaFeeder.java
+++ RookieCoding/src/main/java/frc/robot/subsystems/SecondZophyaFeeder.java
@@ -8,35 +8,32 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 
+public class SecondZophyaFeeder extends SubsystemBase {
+  /** Creates a new SecondZophyaFeeder. */
+  private TalonFX secondFeederMotor;
 
-public class ZophyaFeeder extends SubsystemBase {
-/** Creates a new Feeder */
-private TalonFX feederMotor;
-
-public ZophyaFeeder() {
-    feederMotor = new TalonFX(1);
+  
+public SecondZophyaFeeder(){
+  secondFeederMotor = new TalonFX(1);
 }
 
 public void feederSpinMotor() {
-    feederMotor.setVoltage(4);
+  secondFeederMotor.setVoltage(4);
 }
 
 public void feederStopMotor() {
-    feederMotor.setVoltage(0);
+  secondFeederMotor.setVoltage(0);
 }
 
 public void reverseFeederMotor(){
-    feederMotor.setVoltage(-4);
+  secondFeederMotor.setVoltage(-4);
 }
-
+ 
 public void spinFeederMotor(double speed) {
-    feederMotor.setVoltage(speed);
+  secondFeederMotor.setVoltage(speed);
 }
 
 @Override
 public void periodic() {
 }
-
 }
-
-
