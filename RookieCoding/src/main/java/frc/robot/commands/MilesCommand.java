c55e00757215b43cc937f52d9ba6b41c881b7486
Miles E <armaroug3@gmail.com>
2024-11-21 16:53:10 -0800

    made parameter variable

--- RookieCoding/src/main/java/frc/robot/commands/MilesCommand.java
+++ RookieCoding/src/main/java/frc/robot/commands/MilesCommand.java
@@ -11,11 +11,13 @@ public class MilesCommand extends Command {
 
 private MilesSubsystem mMilesMotor;
 private MilesSubsystem mMilesMotorNegative;
+private MilesSubsystem mVariableVariable;
   /** Creates a new MilesCommand. */
-  public MilesCommand(MilesSubsystem MilesMotor, MilesSubsystem MilesMotorNegative) {
+  public MilesCommand(MilesSubsystem MilesMotor, MilesSubsystem MilesMotorNegative, int variableVariable) {
    this.mMilesMotor = MilesMotor;
    this.mMilesMotorNegative = MilesMotorNegative; 
-  }
+   this.mVariableVariable = variableVariable;}
+
 
   // Called when the command is initially scheduled.
   @Override
@@ -23,13 +25,13 @@ private MilesSubsystem mMilesMotorNegative;
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {mMilesMotor.MilesVariableLoop(); mMilesMotorNegative.MilesVariableLoopNegative();}
+  public void execute() {mMilesMotor.milesMotorMasterLoop(); mMilesMotorNegative.milesMotorMasterLoop();}
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {MilesMotor.milesMotorMasterLoop();}
 
-  // Returns true when the command should end.
+  // Returns true when the command should end.0o
   @Override
   public boolean isFinished() {
     return false;
