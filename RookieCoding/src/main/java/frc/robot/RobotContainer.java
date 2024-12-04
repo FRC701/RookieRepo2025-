2449628dcdb0dd217c950d7e92d58c52591bf651
Amelie Martin <ameliem@vandenrobotics.com>
2024-12-03 16:38:45 -0800

    Made a button in robot container

--- RookieCoding/src/main/java/frc/robot/RobotContainer.java
+++ RookieCoding/src/main/java/frc/robot/RobotContainer.java
@@ -8,9 +8,12 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.subsystems.Somthing;
+import frc.robot.commands.Motor;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -21,6 +24,11 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private  Somthing mSomthing2 = new Somthing();
+
+ private final CommandXboxController driver =
+ new CommandXboxController(OperatorConstants.kDriverControllerPort);
+
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
@@ -30,6 +38,7 @@ public class RobotContainer {
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
+
   }
 
   /**
@@ -42,15 +51,19 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
+    driver.y().onTrue(new Motor(mSomthing2, 93408000));
+
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
     new Trigger(m_exampleSubsystem::exampleCondition)
         .onTrue(new ExampleCommand(m_exampleSubsystem));
 
+    
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
     m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
   }
-
+   
+  
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
