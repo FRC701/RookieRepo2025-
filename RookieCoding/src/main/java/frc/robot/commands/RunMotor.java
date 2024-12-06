f24eb483b7787ac88c7835dcf89925c64048a8cd
Amelie Martin <ameliem@vandenrobotics.com>
2024-12-05 16:36:21 -0800

    Attempted to make instant command. Doesn't work yet :(

---
+++ RookieCoding/src/main/java/frc/robot/commands/RunMotor.java
@@ -0,0 +1,29 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import javax.management.RuntimeErrorException;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class RunMotor extends InstantCommand {
+  private SomthingSubsystem mSomthingSubsystem = new ();
+  private double speed;
+  public RunMotor(Somthing AmelieMotor, double speed) {
+    this.mSomthingSubsystem = AmelieMotor;
+    this.speed = speed;
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    
+    
+  }
+}
